21848f0d27e42855f94d7261635196a186ee9625
HanaPark1 <ydjxlst@gmail.com>
2025-08-08 14:35:51 +0900

    study: formdata 파라미터로 전달받기

---
+++ w0807/src/main/java/com/java/ex/FormPage2.java
@@ -0,0 +1,50 @@
+package com.java.ex;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FormPage2")
+public class FormPage2 extends HttpServlet {
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		String name = request.getParameter("name");
+		int kor = Integer.parseInt(request.getParameter("kor"));
+		int eng = Integer.parseInt(request.getParameter("eng"));
+		int math = Integer.parseInt(request.getParameter("math"));
+		int total = kor+eng+math;
+		double avg = total / 3.0;
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<meta charset=\"UTF-8\">");
+		writer.println("<title>응답페이지</title>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h2>입력정보</h2>");
+		writer.println("<p>이름: "+name+"</p>");
+		writer.println("<p>국어: "+kor+"</p>");
+		writer.println("<p>영어: "+eng+"</p>");
+		writer.println("<p>수학: "+math+"</p>");
+		writer.println("<p>합계: "+total+"</p>");
+		writer.println("<p>평균: "+avg+"</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request,response);
+	}
+
+}
